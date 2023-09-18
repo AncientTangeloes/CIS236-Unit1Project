22945777655da6c76cabebd533b0a26c41c00ba2
AncientTangeloes <119451157+AncientTangeloes@users.noreply.github.com>
2023-09-17 23:07:19 -0500

    Add project files.

---
+++ Unit1Project-HarryByrd/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unit1Project_HarryByrd.Models;
+
+namespace Unit1Project_HarryByrd.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.U1P = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Unit1ProjectModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.U1P = model.GenerateMemeName();
+            }
+            else
+            {
+                ViewBag.U1P = "";
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Unit1Project-HarryByrd/Models/Unit1ProjectModel.cs
@@ -0,0 +1,102 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Unit1Project_HarryByrd.Models
+{
+    public class Unit1ProjectModel
+    {
+        [Required(ErrorMessage = "Please enter a number.")]
+        public int? NumberInput { get; set; }
+        [Required(ErrorMessage = "Please enter a letter.")]
+        public string? LetterInput { get; set; }
+        public string? GenerateMemeName()
+        {
+            string? number = "ERROR";
+            if (NumberInput == 0)
+            {
+                number = "Big";
+            }
+            else if (NumberInput == 1)
+            {
+                number = "Robbie";
+            }
+            else if (NumberInput == 2)
+            {
+                number = "Sonic";
+            }
+            else if (NumberInput == 3)
+            {
+                number = "Quandale";
+            }
+            else if (NumberInput == 4)
+            {
+                number = "Michael";
+            }
+            else if (NumberInput == 5)
+            {
+                number = "King";
+            }
+            else if (NumberInput == 6)
+            {
+                number = "I.M.";
+            }
+            else if (NumberInput == 7)
+            {
+                number = "Spongebob";
+            }
+            else if (NumberInput == 8)
+            {
+                number = "Barry B.";
+            }
+            else if (NumberInput == 9)
+            {
+                number = "Ugandan";
+            }
+
+            string? letter = "ERROR";
+            if (LetterInput.ToLower() == "a")
+            {
+                letter = "Chungus";
+            }
+            else if (LetterInput.ToLower() == "b")
+            {
+                letter = "Rotten";
+            }
+            else if (LetterInput.ToLower() == "c")
+            {
+                letter = "the Hedgehog";
+            }
+            else if (LetterInput.ToLower() == "d")
+            {
+                letter = "Dingle";
+            }
+            else if (LetterInput.ToLower() == "e")
+            {
+                letter = "Rosen";
+            }
+            else if (LetterInput.ToLower() == "f")
+            {
+                letter = "Harkinian";
+            }
+            else if (LetterInput.ToLower() == "g")
+            {
+                letter = "Meen";
+            }
+            else if (LetterInput.ToLower() == "h")
+            {
+                letter = "Squarepants";
+            }
+            else if (LetterInput.ToLower() == "i")
+            {
+                letter = "Benson";
+            }
+            else if (LetterInput.ToLower() == "j")
+            {
+                letter = "Knuckles";
+            }
+
+            string? memeName = number + " " + letter;
+            
+            return memeName;
+        }
+    }
+}
